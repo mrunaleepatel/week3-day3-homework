e769813bf626e330dd48c8a5a588ca160cfdacf4
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-05-15 19:47:26 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -12,5 +12,27 @@ $(() => {
   
     // Append the $h1 element to the $container
     $container.append($h1);
-  });
-  
+
+
+    // Add h2 element with your name
+    const $h2 = $('<h2>');
+    $h2.text('Your Name');
+    $container.append($h2);
+
+    // Add h3 element with your house
+    const $h3 = $('<h3>');
+    $h3.text('House: Hufflepuff');
+    $container.append($h3);
+
+    // Add h4 element with your pet's name and a class matching your pet type
+    const $h4Pet = $('<h4>');
+    $h4Pet.text('Pet Name: Lily');
+    $h4Pet.addClass('owl'); 
+    $container.append($h4Pet);
+
+    // Add h4 element with your wand description
+    const $h4Wand = $('<h4>');
+    $h4Wand.text('Wand: Unicorn hair');
+    $container.append($h4Wand); 
+
+});
